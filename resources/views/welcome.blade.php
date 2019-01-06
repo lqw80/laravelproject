8884dff9bbb510a2be55f46b7f4401e633b80260
lqw <lqwbig@163.com>
2019-01-06 21:28:34 +0800

    modified:   readme.md 	modified:   resources/views/welcome.blade.php 	modified:   README.md

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    我操！
                 </div>
 
                 <div class="links">
